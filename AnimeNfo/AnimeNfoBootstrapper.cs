f7eb9213fd71fff0cdad9fad8381f397d88fe066
Przemysław Lewandowski <prz.lewandowski@gmail.com>
2013-02-06 20:37:38 +0100

    Added prism initialization.

---
+++ AnimeNfo/AnimeNfoBootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.ServiceLocation;
+
+namespace AnimeNfo
+{
+    public class AnimeNfoBootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return ServiceLocator.Current.GetInstance<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow = (Window) Shell;
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            RegisterTypeIfMissing(typeof(MainWindow), typeof(MainWindow), false);
+        }
+    }
+}
